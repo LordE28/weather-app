a96062979beb7b8b8ee14387e08914c87a7bc1c1
Riyaad <riyaadely@gmail.com>
2020-01-08 12:55:37 +0200

    my project

---
+++ src/components/test/Form.test.js
@@ -0,0 +1,12 @@
+//import React from "react";
+//import ReactDOM from "react-dom";
+//import Form from "./components/Form";
+
+
+
+//it('renders correctly', () => {
+  //  const tree = renderer
+    //.create(<Form/>)
+    //.toJSON();
+    //expect(tree).toMatchSnapshot();
+    //});
